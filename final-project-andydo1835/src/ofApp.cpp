003917c98219538863782a5c962739e00eabfdcb
andydo1835 <tungmd2@illinois.edu>
2019-04-28 17:27:41 -0500

    Implemented pausing, code cleanup

--- final-project-andydo1835/src/ofApp.cpp
+++ final-project-andydo1835/src/ofApp.cpp
@@ -1,11 +1,9 @@
 #include "ofApp.h"
 #include <stdlib.h>
 
-//--------------------------------------------------------------
 void ofApp::setup(){
 
 	int bufferSize = 512;
-	delay.setDelay(8192);
 	ofSoundStreamSettings streamSettings;
 		streamSettings.numInputChannels = 2;
 		streamSettings.numOutputChannels = 2;
@@ -22,67 +20,181 @@ void ofApp::setup(){
 
 	ofFileDialogResult result = ofSystemLoadDialog("Please select an audio file (.mp3, .wav, .aiff, .aac)");
 	if (result.bSuccess) {
-		std::cout << "Path: " << result.getPath() << std::endl;
 		song.load(result.getPath());
 		player.load(result.getPath());
-		std::cout << "Load Successful!" << std::endl;
 		song.play();
 		song.setVolume(0);
 	}
 
-	song.connectTo(song_output).connectTo(delay);
+	song.connectTo(song_output);
 	player.connectTo(player_output);
 
 	rms_values.push_back(0.0f);
 
-	ofBackground(0, 0, 0);
+	ofBackground(ofColor::black);
 
-	for (int i = 0; i < 4; i++) {
-		ofRectangle key(256 * i, 64, 256, 96);
+	for (int i = 0; i < NUM_KEYS; i++) {
+		ofRectangle key(ofGetWindowWidth() / NUM_KEYS * i, NOTE_HEIGHT * 2,		// (x, y) coordinates
+						ofGetWindowWidth() / NUM_KEYS, NOTE_HEIGHT * 3);	    // (l, h) dimensions
 		keys.push_back(key);
 	}
-	ofSetColor(256, 256, 256);
-	key_pressed.resize(keys.size(), false);
+	ofSetColor(ofColor::white);
+	key_pressed.resize(keys.size() + 1, false);
+
+	gui.setup();
+	gui.add(scroll_speed.setup("scroll speed", DEFAULT_SCROLL_SPEED, MIN_SCROLL_SPEED, MAX_SCROLL_SPEED));
+	gui.add(difficulty.setup("difficulty", DEFAULT_DIFFICULTY, MIN_DIFFICULTY, MAX_DIFFICULTY));
+
+	keybindings.insert(pair<char, int>('w', 0));
+	keybindings.insert(pair<char, int>('e', 1));
+	keybindings.insert(pair<char, int>('i', 2));
+	keybindings.insert(pair<char, int>('o', 3));
+	keybindings.insert(pair<char, int>('p', 4));
 }
 
-//--------------------------------------------------------------
 void ofApp::update(){
-	count++;
-	if (count == 700 / scroll_speed * -1) {
-		player.play();
-		started = true;
-	}
+	song.setPaused(paused);
+	player.setPaused(paused);
+	if (paused) {
 
-	// add the current rms value
-	rms = song_output.getBuffer().getRMSAmplitude();
-	rms_values.push_back(rms);
-
-	// we can "smooth" the rms value by using feedback.
-	// this is a simple example of using the previous value 
-	// with a weight (0.9) and the current value (0.1)
-
-	//rms_values.push_back(0.9 * rms_values.back() + 0.1 * rms);
-
-	// we only keep a maximum of 25 values of the rms readings
-	if (rms_values.size() > 25) {
-		// if we have added more than 25, then we delete the first one
-		rms_values.erase(rms_values.begin(), rms_values.begin() + 1);
-	}
-
-	if (!player.isPlaying() && started) {
-		ofExit();
+	} else {
+		updateRMS();
+		detectAudioOnset();
+		trackGameState();
 	}
 }
 
-//--------------------------------------------------------------
 void ofApp::draw(){
 	std::cout << score << std::endl;
+	if (paused) {
+		gui.draw();
+	} else {
+		drawKeys();
+		drawNotes();
+	}
+}
+
+void ofApp::keyPressed(int key){
+	if (keybindings.count(key) == 0) {
+		return;
+	}
+
+	int pressed_index = keybindings[key];
+
+	if (!key_pressed[pressed_index]) {
+		if (pressed_index < NUM_KEYS) {
+			checkHit(pressed_index);
+		} else {
+			if (paused) {
+				paused = false;
+			} else {
+				paused = true;
+			}
+		}
+		key_pressed[pressed_index] = true;
+	}
+}
+
+void ofApp::keyReleased(int key){
+	if (keybindings.count(key) == 0) {
+		return;
+	}
+
+	key_pressed[ keybindings[key] ] = false;
+}
+
+bool ofApp::checkHit(int pos) {
+	for (int i = 0; i < notes.size(); i++) {
+		if (notes[i].intersects(keys[pos])) {
+			notes.erase(notes.begin() + i);
+			combo++;
+			score += combo;
+			return true;
+		}
+	}
+	combo = 0;
+	return false;
+}
+
+void ofApp::makeNote() {
+	int column = 0;
+	while (column == last_column) {
+		column = rand() % NUM_KEYS;
+	}
+	last_column = column;
+	bool intersect = false;
+	ofRectangle note = ofRectangle(ofGetWindowWidth() / NUM_KEYS * column,			// x coordinate
+								   ofGetWindowHeight(),								// y coordinate
+								   ofGetWindowWidth() / NUM_KEYS, NOTE_HEIGHT);	    // (l, h) dimensions
+	for (int i = 0; i < notes.size(); i++) {
+		// if it intersects another rectangle, don't draw it
+		if (note.intersects(notes[i])) {
+			intersect = true;
+			break;
+		}
+	}
+	if (!intersect) {
+		notes.push_back(note);
+	}
+}
+
+bool ofApp::checkStart() {
+	// here, we make the assumption that the song creates a note
+	// to start, which is usually true unless it begins extremely quietly
+	for (int i = 0; i < notes.size(); i++) {
+		if (notes[i].getTop() <= NOTE_HEIGHT * 5) { // NOTE_HEIGHT * 5 = bottom of the row of keys
+			started = true;
+			player.play();
+		}
+	}
+	return started;
+}
+
+void ofApp::drawNotes() {
+	for (int i = 0; i < notes.size(); i++) {
+		notes[i].translateY(scroll_speed * -1);
+		ofDrawRectangle(notes[i]);
+
+		// if the player has missed a note
+		if (notes[i].getTop() <= 0) {
+			score--;
+			notes.erase(notes.begin() + i);
+			combo = 0;
+		}
+	}
+}
+
+void ofApp::drawKeys() {
 	for (int i = 0; i < keys.size(); i++) {
 		ofNoFill();
 		ofDrawRectangle(keys[i]);
 	}
 	ofFill();
+}
 
+void ofApp::updateRMS() {
+	// add the current rms value
+	rms = song_output.getBuffer().getRMSAmplitude();
+	rms_values.push_back(rms);
+
+	if (rms_values.size() > RMS_BUFFER) {
+		// if we have added more than rms_buffer rms values, then we delete the first one
+		rms_values.erase(rms_values.begin(), rms_values.begin() + 1);
+	}
+}
+
+bool ofApp::trackGameState() {
+	if (!player.isPlaying() && started) {
+		ofExit();
+	}
+
+	if (!started) {
+		checkStart();
+	}
+	return started;
+}
+
+bool ofApp::detectAudioOnset() {
 	// calculate the average of the rms values
 	float average_rms = 0.0f;
 	for (int i = 0; i < rms_values.size(); i++) {
@@ -97,141 +209,15 @@ void ofApp::draw(){
 	}
 	var_rms = var_rms / rms_values.size();
 
-	// now we see if the current value is outside the mean + variance
-	// basic statistics tells us a normally distributed function
-	// has a mean and a variance where 97% of the data is explained by
-	// 3 standard deviations.  we use this principle here in detecting 
-	// the the current rms reading is outside this probability
-	if (rms_values.back() > (average_rms + 4.0*var_rms)) {
-		int column = rand() % 4;
-		bool intersect = false;
-		ofRectangle* myRect = new ofRectangle(256 * column, 768, 256, 32);
-		for (int i = 0; i < notes.size(); i++) {
-			if (myRect->intersects(*notes[i])) {
-				delete myRect;
-				intersect = true;
-				break;
-			}
-		}
-		if (!intersect) {
-			notes.push_back(myRect);
-		}
+	/*
+	* if the current rms value is "difficulty" variances
+	* away from the average, we can be so sure that there is a
+	* significant change in the rms values, therefore we've
+	* detected an audio onset
+	*/
+	if (rms_values.back() > (average_rms + (6.0 - difficulty) * var_rms)) {
+		makeNote();
+		return true;
 	}
-
-	for (int i = 0; i < notes.size(); i++) {
-		if (notes[i]->getTop() <= 0) {
-			delete notes[i];
-			notes.erase(notes.begin() + i);
-			score--;
-		} else {
-			notes[i]->translateY(scroll_speed);
-			ofDrawRectangle(*notes[i]);
-		}
-	}
-}
-
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-	switch (key) {
-	case 'w':
-		if (!key_pressed[0]) {
-			checkHit(0);
-			key_pressed[0] = true;
-		}
-		break;
-	case 'e':
-		if (!key_pressed[1]) {
-			checkHit(1);
-			key_pressed[1] = true;
-		}
-		break;
-	case 'i':
-		if (!key_pressed[2]) {
-			checkHit(2);
-			key_pressed[2] = true;
-		}
-		break;
-	case 'o':
-		if (!key_pressed[3]) {
-			checkHit(3);
-			key_pressed[3] = true;
-		}
-		break;
-	}
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-	switch (key) {
-	case 'w':
-		key_pressed[0] = false;
-		break;
-	case 'e':
-		key_pressed[1] = false;
-		break;
-	case 'i':
-		key_pressed[2] = false;
-		break;
-	case 'o':
-		key_pressed[3] = false;		
-		break;
-	}
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
-}
-
-bool ofApp::checkHit(int pos) {
-	for (int i = 0; i < notes.size(); i++) {
-		if (notes[i]->intersects(keys[pos])) {
-			delete notes[i];
-			notes.erase(notes.begin() + i);
-			score++;
-			return true;
-		}
-	}
-	score--;
 	return false;
 }
--- final-project-andydo1835/src/ofApp.h
+++ final-project-andydo1835/src/ofApp.h
@@ -1,30 +1,46 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxGui.h"
 #include "ofxSoundPlayerObject.h"
-#include "DigitalDelay.h"
+#include <map>
+
+using std::map;
+using std::pair;
+
+constexpr float MIN_DIFFICULTY = 1.0;
+constexpr float MAX_DIFFICULTY = 5.0;
+constexpr float DEFAULT_DIFFICULTY = 3.0;
+constexpr int MIN_SCROLL_SPEED = 5;
+constexpr int MAX_SCROLL_SPEED = 30;
+constexpr int DEFAULT_SCROLL_SPEED = 10;
+
+constexpr int NUM_KEYS = 4;
+constexpr int NOTE_HEIGHT = 32;
+constexpr int RMS_BUFFER = 25;
 
 class ofApp : public ofBaseApp{
 
 	public:
+
+		// default openFrameworks functions
 		void setup();
 		void update();
 		void draw();
-
 		void keyPressed(int key);
 		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
 
+		// gameplay functions
 		bool checkHit(int pos);
+		void makeNote();
+		bool checkStart();
+		void drawNotes();
+		void drawKeys();
+		void updateRMS();
+		bool trackGameState();
+		bool detectAudioOnset();
 
+		// sound objects
 		ofSoundStream song_stream;
 		ofSoundStream player_stream;
 		ofxSoundOutput song_output;
@@ -32,17 +48,24 @@ class ofApp : public ofBaseApp{
 		ofxSoundInput input;
 		ofxSoundPlayerObject song;
 		ofxSoundPlayerObject player;
-		DigitalDelay delay;
 
+		// audio onset detection
 		float rms;
 		vector<float> rms_values;
 
-		bool started = false;
-		vector<ofRectangle*> notes;
-		vector<ofRectangle> keys;
-		int scroll_speed = -10;
+		// game setup / settings
+		ofxPanel gui;
+		ofxIntSlider scroll_speed;
+		ofxFloatSlider difficulty;
+		map<char, int> keybindings;
 
-		int count = 0;
+		// gameplay variables
+		bool started = false;
+		bool paused = false;
+		vector<ofRectangle> notes;
+		vector<ofRectangle> keys;
 		int score = 0;
+		int last_column = 0;
+		int combo = 0;
 		vector<bool> key_pressed;
 };
